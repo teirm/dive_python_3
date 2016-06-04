7f78745a2e0b505e49d0e2df84ae30477d4a0cf4
teirm <tribunus.vii@gmail.com>
2016-06-04 19:59:15 -0400

    Learning generators

--- ch_6/build_match_and_apply.py
+++ ch_6/build_match_and_apply.py
@@ -2,7 +2,6 @@
 
 import re
 
-
 def build_match_and_apply_functions(pattern, search, replace):
     def matches_rule(word):
         return re.search(pattern, word)
@@ -10,19 +9,15 @@ def build_match_and_apply_functions(pattern, search, replace):
         return re.sub(search, replace, word)
     return (matches_rule, apply_rule)
 
-patterns = \
-    (
-        ('[sxz]$',              '$',    'es'),
-        ('[^aeioudgkprt]h$',    '$',    'es'),
-        ('(qu|[^aeiou])y$',      'y$',   'ies'),
-        ('$',                   '$',    's'),
-    )
+def rules(rules_filename):
+    with open(rules_filename, encoding='utf-8') as pattern_file:
+        for line in pattern_file:
+            pattern, search, replace = line.split(None, 3)
+            yield build_match_and_apply_functions(pattern, search, replace)    
 
-rules = [build_match_and_apply_functions(pattern, search, replace)
-            for (pattern, search, replace) in patterns]
-
-def plural(noun):
-    for matches_rule, apply_rule in rules:
+def plural(noun, rules_filename='plural5-rules.txt'):
+    for matches_rule, apply_rule in rules(rules_filename):
         if matches_rule(noun):
             return apply_rule(noun)
+    raise ValueError('no matching rule for {0}'.format(noun))
       
